5a04fd92ad1e9b12f3d2e3551b4e7cfe186af7f5
efratBezalel <efrat.8963@gmail.com>
2025-11-24 15:47:25 +0200

    בניית מחלקת Task

--- src/Task.java
+++ src/Task.java
@@ -1,3 +1,61 @@
 public class Task {
 
+    private static int nextId = 1; // משתנה סטטי
+    private int id;
+    private String title;
+    private String description;
+    private Status status;
+
+    public Task(String title, String description, Status status) {
+        this.id = nextId++;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
