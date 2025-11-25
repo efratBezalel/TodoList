6a59598847aad18238ac360505092e25c0db278f
efratBezalel <efrat.8963@gmail.com>
2025-11-25 14:52:39 +0200

    בנית TaskService  ופונקציית סיום משימה

--- src/TaskService.java
+++ src/TaskService.java
@@ -2,5 +2,10 @@ import java.util.List;
 
 public class TaskService {
 
+    // פונקצית סיום משימה
+    public void EndTask(int id) {
+        updateById(id, "", "" ,Status.DONE); // זה המבנה של פונקציית עדכון
+    }
 
+ 
 }
